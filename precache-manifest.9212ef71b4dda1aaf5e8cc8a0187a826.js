f02084806afd647206d300c64b0a3a5813a5e335
vale75020 <valentinosuma@hotmail.com>
2019-06-28 13:27:28 +0200

    Updates

---
+++ precache-manifest.9212ef71b4dda1aaf5e8cc8a0187a826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecae429071f7dbd6dc51696f34cef76",
+    "url": "/state-management/index.html"
+  },
+  {
+    "revision": "fc78b0af1a9317ee089d",
+    "url": "/state-management/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/state-management/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "fc78b0af1a9317ee089d",
+    "url": "/state-management/static/js/main.32d7cece.chunk.js"
+  },
+  {
+    "revision": "2bc3650d972f651f9922",
+    "url": "/state-management/static/js/runtime~main.24408e17.js"
+  }
+]);
